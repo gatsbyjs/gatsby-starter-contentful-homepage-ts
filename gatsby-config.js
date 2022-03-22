b82cf377efe00bc8dfa3a4362762f498414e067c
Alex Reed <alex.reed@gatsbyjs.com>
2022-03-22 15:25:59 -0400

    add yarn.lock file to list of files copied over in publish to ensure working dependency versions are installed

---
+++ gatsby-config.js
@@ -0,0 +1,42 @@
+// support for .env, .env.development, and .env.production
+require("dotenv").config()
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://gatsbycontentfulhomepage.gatsbyjs.io/",
+    title: "Gatsby Contentful Homepage Starter",
+    author: `Gatsby`,
+    description: "A Gatsby Starter for building homepages with Contentful",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        downloadLocal: true,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        host: process.env.CONTENTFUL_HOST,
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-image",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-vanilla-extract",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Gatsby Starter Contentful Homepage",
+        short_name: "Gatsby",
+        start_url: "/",
+        // These can be imported once ESM support lands
+        background_color: "#ffe491",
+        theme_color: "#004ca3",
+        icon: "src/favicon.png",
+      },
+    },
+  ],
+}
